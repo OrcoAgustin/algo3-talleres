e80221666a95183061c4dc539f5bbedfc2d02fba
Agustin <agustinorco@gmail.com>
2025-09-04 15:33:36 -0300

    v2 creo que funciona

--- ej 3/Main.java
+++ ej 3/Main.java
@@ -16,26 +16,60 @@ public class Main {
         //printer
         PrintWriter printer = new PrintWriter(bw);
     
-        ArrayList<Long> lista = new ArrayList<>();
-
-        lista.add(scanner.nextLong()); //n
+        long n = scanner.nextLong();
         int l = scanner.nextInt();
         int r = scanner.nextInt();
-        if(lista.get(0)!=0){
-        //operamos y devuelve lista "limpia"
-        lista = operarLista(lista,printer);
-
-        //contamos
-        printer.print(contar(lista, (l-1), r));
-        printer.flush();
-        }
-        else{
-            //ahorra tiempo en caso borde
-            printer.print(0);
-            printer.flush();
-        }
+        
+        printer.print(contar(n,l,r,(long) 1,obtenerLargo(n)));       
     }
 
+    
+    public static Long obtenerLargo(long n){
+        if(n<=1){
+            return (long) 1;
+        }
+        return obtenerLargo(obtenerLargo(n/2)+1+obtenerLargo(n/2));
+    }
+   
+    public static Long contar(Long n, Integer l, Integer r, Long bordeIzq, Long bordeDer) {
+        //si los bordes pedidos l y r caen fuera del numero que estamos operando
+        if((l<bordeIzq)||(r>bordeDer)){
+            return (long) 0;
+        }
+        //casos base
+        if(n<=1){
+
+            if((n==1) && (bordeIzq>=l) && (bordeDer<=r)){
+                return (long) 1;
+            }
+            else{
+            return (long) 0;
+            }
+        }
+        //paso recursivo
+        long medio = bordeIzq+ obtenerLargo(n/2);
+        long res = 0;
+
+        //yendo por izquierda 
+        res +=contar(n/2,l,r,bordeIzq,medio-1);
+        //por derecha
+        res +=contar(n/2,l,r,medio+1,bordeDer);
+        //caso del medio
+        if(l<=medio && medio<=r){
+            res+=1;
+        }
+        return res;
+    }
+
+
+    //esto da problemas al iterar toda la lista se puede hacer muy costoso
+    //tratar de buscar alguna estructura mas util, quiza arbol?
+
+    //cada caso genera f1(i) + f2(i) +f1(i) ent, puedo usar memorizacion para ahorrar calculo ?(no se que tan correcto es usarlo pero vamos a testear)
+    
+    //capaz hacer algo con el arbol sea mejor, sabiendo los bordes podemos buscar algo que interseque y solo operar ahi?
+    //f recursiva me dice largo, de ahi solo creo lista para los que interseque?
+    /* 
     public static ArrayList<Long> operarLista (ArrayList<Long> lista, PrintWriter printer){
         boolean cambio = true;
         while(cambio){
@@ -46,6 +80,7 @@ public class Main {
                 Long valor = lista.get(i);
 
                 if (valor>1){
+
                     lista.remove(i);
                     lista.add(i, (valor/2));    //entero 1
                     lista.add((i+1), (valor%2)); //resto
@@ -69,4 +104,5 @@ public class Main {
         }
         return suma;
     }
+    */
 }
