62de929954d3c2e6f0ae96d57c819850497b61c0
Agustin <agustinorco@gmail.com>
2025-09-04 02:20:54 -0300

    se agrega ej 1, con funcion para detectar si una cadena es l linda, terminar ej para determinar movimientos necesarios para convertir en l lindo

--- ej 1/Main.java
+++ ej 1/Main.java
@@ -1,55 +1,70 @@
-    import java . io .*;
-    import java . util .*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
-    public class Main {
-        public static void main ( String [] args ) {
-            //lector de entrada
-            BufferedReader br = new BufferedReader( 
-                new InputStreamReader(System.in)
-            );
-            //lector de salida
-            BufferedWriter bw = new BufferedWriter (
-                new OutputStreamWriter ( System . out )
-            );
-            //scanner
-            Scanner scanner = new Scanner(br);
-            //printer
-            PrintWriter printer = new PrintWriter(bw);
+public class Main {
+    public static void main ( String [] args ) {
+        //lector de entrada
+        BufferedReader br = new BufferedReader( 
+            new InputStreamReader(System.in)
+        );
+        //lector de salida
+        BufferedWriter bw = new BufferedWriter (
+            new OutputStreamWriter ( System . out )
+        );
+        //scanner
+        Scanner scanner = new Scanner(br);
+        //printer
+        PrintWriter printer = new PrintWriter(bw);
 
-            int a = scanner.nextInt(); //inicio 
-            int b = scanner.nextInt(); //objetivo
-            ArrayList<Integer> hist = new ArrayList<>(); //se guardan aca los valores que van pasando
+        String var = scanner.nextLine();
 
-            aux(a,b,hist, printer);
-
-            printer.flush();
-        }
-
-        //funcion aux//
-        public static void aux(Integer obj, Integer actual, ArrayList<Integer> hist, PrintWriter printer ){
-            hist.add(actual);
-
-            if (obj.equals(actual)){
-                printer.println("YES");
-                printer.println(hist.size());
-                //loop para juntar la respuesta
-                for (int i = (hist.size()-1); i >= 0; i--) {
-                    printer.print(hist.get(i)+" ");
-                }  
-                printer.println(); 
-                return;
-            }
-            if((actual<obj)||(((actual%2)!=0)&&((actual%10)!=1))){
-                printer.println("NO");
-                return;
-            }
-            if((actual%10)==1){
-                aux(obj,(actual-1)/10,hist,printer);
-            }
-            
-            if((actual%2)==0){
-                aux(obj,actual/2,hist,printer);
-            }
-        }
+        // tomo en principio y final de la string valores para ver si algun lado es "l"
+        printer.println("es "+var.charAt(var.length()-1)+ "-lindo: "+esLindo(var,var.charAt(var.length()-1)));
+        printer.println("es "+var.charAt(0)+ "-lindo: "+esLindo(var,var.charAt(0)));
+        printer.flush();
+    
     }
 
+    public static boolean esLindo(String cadena, char letra){
+        //caso base
+        if (cadena.length() == 1){
+            char v = cadena.charAt(0);
+            return (v==letra);
+        }
+        String izq = cadena.substring(0,cadena.length()/2);
+        String der = cadena.substring(cadena.length()/2, cadena.length());
+
+        //caso cadena larga a la izq
+        Boolean izqEsLinda = true;  
+        int i = 0;  
+        while ((izqEsLinda) && (i<izq.length())){
+            if(izq.charAt(i) != letra){
+                izqEsLinda = false;
+            }
+            i+=1;
+        }
+        if((izqEsLinda) && esLindo(der,(char)(letra+1))){
+            return true;
+        }
+        
+        //caso cadena larga a la der
+        Boolean derEsLinda = true;  
+        int j = 0;  
+        while ((derEsLinda) && (j<der.length())){
+            if(der.charAt(j) != letra){
+                derEsLinda = false;
+            }
+            j+=1;
+        }
+        if((derEsLinda) && esLindo(izq,(char)(letra+1))){
+            return true;
+        }
+        
+        //no es ninguna de los 3
+        return false;
+    }
+}
