4b23b12b33df9077fba4d26d9e834e2623a18e4b
Agustin <agustinorco@gmail.com>
2025-09-20 16:08:07 -0300

    codigo pre pruebas

--- taller 2/ej 1/Main.java
+++ taller 2/ej 1/Main.java
@@ -12,7 +12,8 @@ public class Main {
     public static ArrayList<StringBuilder> strings = new ArrayList<StringBuilder>();
     public static ArrayList<StringBuilder> stringsInvertidos = new ArrayList<StringBuilder>();
     public static ArrayList<Integer> costos = new ArrayList<Integer>();
-    public static Integer[][] memo = null;
+    public static Long INF = Long.MAX_VALUE; 
+    public static Long [][] memo = null;
     public static int n = 0;
     
     public static void main ( String [] args ) {
@@ -32,7 +33,7 @@ public class Main {
         //inputs
         n = scanner.nextInt();
         //inicializar memo 
-        memo = new Integer[n][2];
+        memo = new Long[n][2];
 
 
         for (int i = 0; i < n; i++) {
@@ -46,37 +47,62 @@ public class Main {
             strings.add(new StringBuilder(input));
             stringsInvertidos.add(new StringBuilder(input).reverse());
         }
-
         //hasta aca tenemos la lista con todos los strings que se van a usar
         
+        long ans =Math.min(alf((n-1), 0),alf((n-1), 1));
+        if (ans>=INF){
+            printer.print(-1);
+            printer.flush();
+        }else{
+            printer.print(ans);
+            printer.flush();
+        }
     }
 
-    public static Integer alf(int i,int  estado) {
-        if(i==n){
-            if(estado==0){
-                return 0;
-            }
-            else{
-                return costos.get(i-1);
-            }
-        }
+    public static Long alf(Integer  i,Integer estado) {
         if((memo[i][estado])!=null){
             return memo[i][estado];
         }
-
         
+        
+        if(i==0){
+            if(estado==0){
+                return (long) 0;
+            }
+            else{
+                return (long) costos.get(0);
+            }
+        }
 
+        Long mejor = INF;
 
+        if(estado ==0){
+            //si el compareto da <=0 quiere decir que es lexicograficamente meno el primero, 0 si son iguales, >0 si el segundo es mas chico (caso q no queres)  
+            if(((strings.get(i-1)).compareTo(strings.get(i)))<=0){
+                mejor= Math.min(mejor,alf((i-1),0));
+            }
+            if(((stringsInvertidos.get(i-1)).compareTo(strings.get(i)))<=0){
+                mejor= Math.min(mejor,alf((i-1),1));
+            }
+        }else{
+            if(((strings.get(i-1)).compareTo(stringsInvertidos.get(i)))<=0){
+                mejor= Math.min(mejor,alf((i-1),0)+costos.get(i));
+            }
+            if(((stringsInvertidos.get(i-1)).compareTo(stringsInvertidos.get(i)))<=0){
+                mejor= Math.min(mejor,alf((i-1),1)+costos.get(i));
+            }
+        }
 
-
-        return 0;
+        memo[i][estado] = mejor;
+        return mejor;
     }
 
     //usar string builder para rotar arrays
     //para estado (invertido o no) es conv usar un numero en vez de un bool para la matriz
+    // dp[i][estado]= costo min de ordenar correctamente hasta la pos i dejando i en el estado 1 o 0
     //1=inv; 0=normal
+    //la dp es n *2 = O(N)
 
-
-    //PENSA LA F REC NO SEAS VAGO
+    
 }
    
