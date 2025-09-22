e8dd36f176f150c93bdc1c848b265871ac15414b
Agustin <agustinorco@gmail.com>
2025-09-22 12:23:23 -0300

    arranca ej 2

---
+++ taller 2/ej 2/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static Integer[][] memo = null; //tiene l y r para determinar de donde me conviene acceder
+  
+    
+    public static void main ( String [] args ) {
+        //lector de entrada
+        BufferedReader br = new BufferedReader( 
+            new InputStreamReader(System.in)
+        );
+        //lector de salida
+        BufferedWriter bw = new BufferedWriter (
+            new OutputStreamWriter ( System . out )
+        );
+        //scanner
+        Scanner scanner = new Scanner(br);
+        //printer
+        PrintWriter printer = new PrintWriter(bw);
+
+        Integer n = scanner.nextInt();
+        String s = scanner.next();
+
+
+    }
+
+    public static Integer contar (String s ){
+        //casos bases
+        if (s.length()==0){
+            return 0;   
+        }
+        if (s.length()==1){
+            return 1;   
+        }
+
+
+        return 0;
+    }
+}
+   
