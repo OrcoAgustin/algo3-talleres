c1050d5de9ae9920b5b5275396319ae6b6e816d9
Agustin <agustinorco@gmail.com>
2025-10-30 00:35:44 -0300

    casi funciona, mejora tle

--- taller 3/ej 1/Main.java
+++ taller 3/ej 1/Main.java
@@ -4,9 +4,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main ( String [] args ) {
         //lector de entrada
         BufferedReader br = new BufferedReader( 
@@ -23,45 +25,57 @@ public class Main {
 
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-
-        printer.print(bfsGenMod(n, m, printer));
+        
+        printer.println(bfsDesdeRaiz(n, m ));
         printer.flush();
-
-
     }
+    
+    public static int bfsDesdeRaiz(int raiz, int lim){
 
-    public static int bfsGenMod(int raiz , int lim, PrintWriter printer) {
+        HashSet<Integer> memo = new HashSet<>();
+
         boolean alcanzado = false;
 
-        ArrayList<ArrayList<Integer>> arbol = new ArrayList<>();
-        arbol.add(new ArrayList<>());
-        arbol.get(0).add(raiz);
+        ArrayList<Integer> prevNivel = new ArrayList<>();
+        prevNivel.add(raiz);
+        
         //agregamos raiz al arbol
 
-        //crea arbol entero
         int nivel = 0;
+
         while (!alcanzado){
             ArrayList<Integer> nuevoNivel = new ArrayList<>();
-            for (int i = 0; i < (arbol.get(nivel)).size(); i++) {
+            for (int i = 0; i < prevNivel.size(); i++) {
                 
-                Integer nodo = arbol.get(nivel).get(i);
-        
-                nuevoNivel.add(nodo*2);
-                if ((nodo-1)>=1){
-                    nuevoNivel.add(nodo-1);
+                Integer nodo = prevNivel.get(i);
+                
+                if(!memo.contains(i)){
+                    boolean alcanzablePorDerecha = true;
+                    if (nodo > lim){
+                        alcanzablePorDerecha = false;
+                    }
+
+                    if(alcanzablePorDerecha){
+                        nuevoNivel.add(nodo*2);
+                    }
+                    
+                    if (nodo>1){
+                        nuevoNivel.add(nodo-1);
+                    }
+
+                    //se alcanzo?
+                    if (nodo*2 == lim || nodo-1 == lim){
+                        alcanzado=true;
+                    }
                 }
 
-                //se alcanzo?
-                if (nodo*2 == lim || nodo-1 == lim){
-                    alcanzado=true;
-                }
+                memo.add(nodo);
             }   
             
-            arbol.add(nuevoNivel);
-            nivel+=1;
+            prevNivel = nuevoNivel;
+            nivel+=1; 
         }
-        
-        /* 
+        /*
         //print
         for (int idx = 0; idx < arbol.size(); idx++) {
             for (int i = 0; i < arbol.get(idx).size(); i++) {
@@ -73,10 +87,10 @@ public class Main {
         printer.flush();
         
 
-        printer.print("el nivel donde se encontro fue: " + nivel);
+        printer.println("el nivel donde se encontro fue: " + nivel);
         printer.flush();
         */
-        return nivel;  
-        
+
+        return nivel;
     }
 }
