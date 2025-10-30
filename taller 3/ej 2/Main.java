094aab43bd4c7f121fe31c35922a0b7a2183cbcf
Agustin <agustinorco@gmail.com>
2025-10-30 15:59:48 -0300

    la matriz de adyacencia se va a ir de complejidad, lista de adyacencia mejor

--- taller 3/ej 2/Main.java
+++ taller 3/ej 2/Main.java
@@ -5,6 +5,9 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+//import java.util.PriorityQueue;
+//import java.util.Comparator;
+
 
 public class Main {
 
@@ -22,61 +25,115 @@ public class Main {
         //printer
         PrintWriter printer = new PrintWriter(bw);
 
-        //1ro salida, 2do llegada, 3ro w
-        ArrayList<ArrayList<Integer>> matrizDeAristas = new ArrayList<>();
-        //indice es planeta y los valores son los minutos donde esta ocupado
-        ArrayList<ArrayList<Integer>> matrizDeConflictos = new ArrayList<>();
-        
-
         int n = scanner.nextInt();
         int m = scanner.nextInt();
+
+
+        int[][] matrizDeAdyacencia = new int[n][n];
+
+        //indice es planeta-1 y los valores son los momentos  donde esta ocupado
+        ArrayList<Integer>[] matrizDeConflictos = new ArrayList[n];
+
+        for (int i = 0; i < m; i++) {
+            int primerCoord = scanner.nextInt()-1;
+            int segundaCoord = scanner.nextInt()-1;
+
+            matrizDeAdyacencia[primerCoord][segundaCoord] = scanner.nextInt();
+        }
+    
+        //conflictos
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer>  nuevaLinea = new ArrayList<>();
+
+            int cantidadDeConflictos = scanner.nextInt();
+            for (int j = 0; j < cantidadDeConflictos ; j++) {
+                int nuevoNum = scanner.nextInt();
+                nuevaLinea.add(nuevoNum);  
+              
+            }
+
+            matrizDeConflictos[i]=nuevaLinea;
+        }
+
+
+        printer.println(pasosHastaM(matrizDeAdyacencia, matrizDeConflictos));
+        
+        //print de adyacencia
+        printer.println("la matriz de adyacencia se ve asi:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printer.print(matrizDeAdyacencia[i][j]+" ");
+            }
+            printer.println();
+        }
+        printer.flush();
+
+    }
+
+    public static int pasosHastaM(int[][] matrizDeAdyacencia, ArrayList<Integer>[] matrizDeConflictos){
+        //dijkstra v2 (echu's dream)
+        //proba si pasa con matriz, sino sufrimos con la queue
+        
+        for (int i = 0; i < matrizDeAdyacencia.length; i++) {
+            for (int j = 0; j < matrizDeAdyacencia.length; j++) {
+                if (matrizDeAdyacencia[i][j] == 0 && i != j){
+                    matrizDeAdyacencia[i][j] = 10001;
+                }
+            }
+        }
+        ArrayList<Integer> visitados = new ArrayList<>();
+        
+        Integer raiz = matrizDeAdyacencia[0][0];
+        visitados.add(raiz);
+
         
 
+
+        return 0;
+    }
+    
+
+
+
+
+
+
+    
+
+    
+}
+
+
+
+
+
+
+
+/* printer de input
         printer.println("n es: " + n);
         printer.println("m es: " + m);
         printer.flush();
 
-        for (int i = 0; i < m; i++) {
-            ArrayList<Integer>  nuevaArista = new ArrayList<>();
-            for (int j = 0; j < 3; j++) {
-                nuevaArista.add(scanner.nextInt());    
-            }
-            
-            matrizDeAristas.add(nuevaArista);
-        }
 
-        //print
-        printer.println("la matriz de aristas se ve asi:");
-        for (int i = 0; i < matrizDeAristas.size(); i++) {
-            for (int j = 0; j < matrizDeAristas.get(i).size(); j++) {
-                printer.print((matrizDeAristas.get(i)).get(j)+" ");
+        //print de adyacencia
+        printer.println("la matriz de adyacencia se ve asi:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printer.print(matrizDeAdyacencia[i][j]+" ");
             }
             printer.println();
         }
         printer.flush();
 
 
-        //conflictos
-        for (int i = 0; i < n; i++) {
-            ArrayList<Integer>  nuevaLinea = new ArrayList<>();
-
-            for (int j = 0; j < scanner.nextInt(); j++) {
-                int nuevoNum = scanner.nextInt();
-                nuevaLinea.add(nuevoNum);  
-              
-            }
-
-            matrizDeConflictos.add(nuevaLinea);
-        }
-
         //print
         printer.println("la matriz de conflictos se ve asi:");
-        for (int i = 0; i < matrizDeConflictos.size(); i++) {
-            printer.print("i = " + i + " ");
+        for (int i = 0; i < matrizDeConflictos.length; i++) {
+            printer.print("i = " + i + ":");
             
-            ArrayList linea=matrizDeConflictos.get(i);
+            ArrayList linea=matrizDeConflictos[i];
 
-            if(linea.size()!=0){
+            if(!linea.isEmpty()){
                 for (int j = 0; j < linea.size(); j++) {
                     printer.print(linea.get(j) + " ");
                 }
@@ -86,6 +143,5 @@ public class Main {
             printer.println();
         }
         printer.flush();
-    }
-    
-}
+
+        */
