6e1262bc7b2dc017bd74746d2ba018599cf46b97
Agustin <agustinorco@gmail.com>
2025-11-10 11:48:56 -0300

    implementa dijkstra, revisar tle

--- taller 3/ej 2/Main.java
+++ taller 3/ej 2/Main.java
@@ -4,9 +4,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.Scanner;
-//import java.util.PriorityQueue;
-//import java.util.Comparator;
 
 
 public class Main {
@@ -29,19 +28,29 @@ public class Main {
         int m = scanner.nextInt();
 
 
-        int[][] matrizDeAdyacencia = new int[n][n];
+        //aristas
 
-        //indice es planeta-1 y los valores son los momentos  donde esta ocupado
-        ArrayList<Integer>[] matrizDeConflictos = new ArrayList[n];
+        ArrayList<Integer[]>[] listaDeAdyacencia = new ArrayList[n];        
+        for (int i = 0; i < n; i++) {
+            listaDeAdyacencia[i] = new ArrayList<Integer[]>();
+        }
+
 
         for (int i = 0; i < m; i++) {
-            int primerCoord = scanner.nextInt()-1;
-            int segundaCoord = scanner.nextInt()-1;
+            int primerP = scanner.nextInt()-1;
+            int segundoP = scanner.nextInt()-1;
+            Integer costo = scanner.nextInt();
 
-            matrizDeAdyacencia[primerCoord][segundaCoord] = scanner.nextInt();
+            listaDeAdyacencia[primerP].add(new Integer[]{segundoP,costo});
+            listaDeAdyacencia[segundoP].add(new Integer[]{primerP,costo});
         }
     
+
         //conflictos
+
+        //indice es planeta-1 y los valores son los momentos donde esta ocupado
+        ArrayList<Integer>[] matrizDeConflictos = new ArrayList[n];
+        
         for (int i = 0; i < n; i++) {
             ArrayList<Integer>  nuevaLinea = new ArrayList<>();
 
@@ -56,50 +65,70 @@ public class Main {
         }
 
 
-        printer.println(pasosHastaM(matrizDeAdyacencia, matrizDeConflictos));
-        
-        //print de adyacencia
-        printer.println("la matriz de adyacencia se ve asi:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                printer.print(matrizDeAdyacencia[i][j]+" ");
-            }
-            printer.println();
-        }
+        printer.println(pasosHastaM(listaDeAdyacencia, matrizDeConflictos));
         printer.flush();
-
     }
 
-    public static int pasosHastaM(int[][] matrizDeAdyacencia, ArrayList<Integer>[] matrizDeConflictos){
+    public static long pasosHastaM(ArrayList<Integer[]>[] listaDeAdyacencia, ArrayList<Integer>[] matrizDeConflictos){
         //dijkstra v2 (echu's dream)
         //proba si pasa con matriz, sino sufrimos con la queue
-        
-        for (int i = 0; i < matrizDeAdyacencia.length; i++) {
-            for (int j = 0; j < matrizDeAdyacencia.length; j++) {
-                if (matrizDeAdyacencia[i][j] == 0 && i != j){
-                    matrizDeAdyacencia[i][j] = 10001;
+        //yo del futuro: no funciona. Usa queue
+    
+        int n = listaDeAdyacencia.length;
+        //almacenar (tiempo, planeta)
+        PriorityQueue<long[]> restantes = new PriorityQueue<>(
+            (a, b) -> Long.compare(a[0], b[0]) 
+        );
+
+        long[] res = new long[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = Long.MAX_VALUE;
+        }
+        res[0] = 0;
+
+        restantes.add(new long[]{0,0});
+
+        while(!restantes.isEmpty()){
+            
+            long[] actual = restantes.poll();
+            long tiempoActual = (int) actual[0];
+            int planetaActual= (int) actual[1];
+
+            //ya calculado?
+            if(tiempoActual > res[planetaActual]){
+                continue;
+            }
+
+            //llegamos?
+            if(planetaActual == n-1){
+                return tiempoActual;
+            }
+            
+            //calculamos el tiempo de salida
+            long tiempoSalida = tiempoActual;
+            for (int i = 0; i < matrizDeConflictos[planetaActual].size(); i++) {
+                if (tiempoSalida == matrizDeConflictos[planetaActual].get(i)){
+                    tiempoSalida += 1;
+                }
+            }
+
+            for (int i = 0; i < listaDeAdyacencia[planetaActual].size(); i++) {
+                //primero: planeta, segundo: costo de llegar
+                int planetaDestino = (int) listaDeAdyacencia[planetaActual].get(i)[0];
+                int costoDeViaje = (int) listaDeAdyacencia[planetaActual].get(i)[1];
+                 
+                long nuevoTiempoDeLlegada = tiempoSalida + costoDeViaje;
+
+                if (nuevoTiempoDeLlegada < res[planetaDestino]){
+                    res[planetaDestino] = nuevoTiempoDeLlegada;
+                    restantes.add(new long[]{nuevoTiempoDeLlegada,planetaDestino});
                 }
             }
         }
-        ArrayList<Integer> visitados = new ArrayList<>();
-        
-        Integer raiz = matrizDeAdyacencia[0][0];
-        visitados.add(raiz);
 
-        
-
-
-        return 0;
+        //inalcanzable el destino
+        return -1;
     }
-    
-
-
-
-
-
-
-    
-
     
 }
 
